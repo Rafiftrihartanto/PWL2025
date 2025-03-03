8abd3b0c601a0a7429cab63860707ba3d4a20749
Rafif Tri Hartanto <rafiftrihartanto900@gmail.com>
2025-03-03 12:36:40 +0700

    Second commit

---
+++ PWL_Week2/Jobsheet2_PWL_2025/app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function __invoke()
+    {
+        return 'Rafif Tri Hartanto (2241760038)';
+    }
+}
--- PWL_Week2/Jobsheet2_PWL_2025/routes/web.php
+++ PWL_Week2/Jobsheet2_PWL_2025/routes/web.php
@@ -16,21 +16,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () { 
-    return 'Hello World'; 
-    }); 
+use App\Http\Controllers\PhotoController; 
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([ 
+    'index', 'show' 
+    ]); 
+    Route::resource('photos', PhotoController::class)->except([ 
+    'create', 'store', 'update', 'destroy' 
+    ]);
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+
+Route::get('/home', HomeController::class);
+Route::get('/about', AboutController::class);
+Route::get('/articles/{id}', ArticleController::class);
+
 
 Route::get('/world', function () { 
-return 'World'; 
-}); 
-
-Route::get('/greet', function () { 
-    return 'Selamat Datang'; 
-    });
-
-Route::get('/about', function () { 
-    return 'Rafif Tri Hartanto (2241760038)'; 
-    });
+    return 'World'; 
+    }); 
 
  Route::get('/user/{Rafif}', function ($name) { 
     return 'Nama saya '.$name; 
@@ -39,11 +46,6 @@ Route::get('/about', function () {
 Route::get('/posts/{post}/comments/{comment}', function 
 ($postId, $commentId) { 
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
-}); 
-
-Route::get('/articles/{id}', function 
-($id) { 
-    return 'Halaman Artikel dengan ID'.$id; 
 }); 
 
 Route::get('/user/{name?}', function ($name='John') { 
