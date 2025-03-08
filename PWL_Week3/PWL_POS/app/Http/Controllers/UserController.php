f2aa270a9e6ca69f4d6dcdf9e33d27c17cf851ae
Rafif Tri Hartanto <rafiftrihartanto900@gmail.com>
2025-03-08 11:02:58 +0700

    jobsheet 4 praktikum 2.4

--- PWL_Week3/PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_Week3/PWL_POS/app/Http/Controllers/UserController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index()
     {
-        // Hitung jumlah pengguna dengan level_id = 2
-        $jumlah_pengguna = UserModel::where('level_id', 2)->count();
+       $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2,
+            ],
+        );
+        $user ->save();
+        return view('user', ['data' => $user]);
 
-        // Kirim data ke view
-        return view('user', compact('jumlah_pengguna'));
     }
 }
--- PWL_Week3/PWL_POS/resources/views/user.blade.php
+++ PWL_Week3/PWL_POS/resources/views/user.blade.php
@@ -1,19 +1,22 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data User</title>
-</head>
-<body>
-    <h1>Data User</h1>
-    <table border="1" cellpadding="5" cellspacing="0">
-        <tr>
-            <th>Jumlah Pengguna</th>
-        </tr>
-        <tr>
-            <td>{{ $jumlah_pengguna }}</td>
-        </tr>
-    </table>
-</body>
+<!DOCTYPE html> 
+<html>
+    <head>
+        <title>Data User</title>
+    </head>
+    <body>
+        <h1>Data User</h1>
+        <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>ID Level Pengguna</th>
+            </tr>
+            <tr>
+                <td>{{$data-> user_id}}</td>
+                <td>{{$data-> username}}</td>
+                <td>{{$data-> nama}}</td>
+                <td>{{$data-> level_id}}</td>
+            </tr>
+        </table>
+    </body>
 </html>
